9c655896a7e4a01ab6a5900d2519fa61e75bde2d
myrdinaptaliesen <64499780+myrdinaptaliesen@users.noreply.github.com>
2022-07-06 09:10:44 +0100

    Debug du 05/07/2022

--- app/Http/Controllers/API/PlayerController.php
+++ app/Http/Controllers/API/PlayerController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
 use App\Models\Player;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class PlayerController extends Controller
 {
@@ -15,7 +16,24 @@ class PlayerController extends Controller
      */
     public function index()
     {
-        //
+        /***** 1ère Méthode *****/
+        // On récupère tous les joueurs
+        // $players = Player::all();
+
+        // On retourne les informations des utilisateurs en JSON
+        // return response()->json($players);
+
+        /***** 2ème Méthode *****/
+        // On récupère tous les joueurs
+        $players = DB::table('players')
+            ->get()
+            ->toArray();
+
+        // On retourne les informations des utilisateurs en JSON
+        return response()->json([
+            'status' => 'Success',
+            'data' => $players,
+        ]);
     }
 
     /**
@@ -26,7 +44,29 @@ class PlayerController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $request->validate([
+            'firstName' => 'required|max:100',
+            'lastName' => 'required|max:100',
+            'height' => 'required|max:100',
+            'position' => 'required|max:100',
+        ]);
+
+        
+
+        // On crée un nouvel utilisateur
+        $player = Player::create([
+            'firstName' => $request->firstName,
+            'lastName' => $request->lastName,
+            'height' => $request->height,
+            'position' => $request->position,
+        ]);
+
+        // On retourne les informations du nouvel utilisateur en JSON
+        return response()->json([
+            'status' => 'Success',
+            'data' => $player,
+          ]);
     }
 
     /**
@@ -37,7 +77,8 @@ class PlayerController extends Controller
      */
     public function show(Player $player)
     {
-        //
+        // On retourne les informations de l'utilisateur en JSON
+        return response()->json($player);
     }
 
     /**
@@ -49,7 +90,23 @@ class PlayerController extends Controller
      */
     public function update(Request $request, Player $player)
     {
-        //
+        $this->validate($request, [
+            'firstName' => 'required|max:100',
+            'lastName' => 'required|max:100',
+            'height' => 'required|max:100',
+            'position' => 'required|max:100',
+        ]);
+
+        // On crée un nouvel utilisateur
+        $player->update([
+            'firstName' => $request->firstName,
+            'lastName' => $request->lastName,
+            'height' => $request->height,
+            'position' => $request->position,
+        ]);
+
+        // On retourne les informations du nouvel utilisateur en JSON
+        return response()->json($player, 201);
     }
 
     /**
@@ -60,6 +117,10 @@ class PlayerController extends Controller
      */
     public function destroy(Player $player)
     {
-        //
+        // On supprime l'utilisateur
+        $player->delete();
+
+        // On retourne la réponse JSON
+        return response()->json();
     }
 }
