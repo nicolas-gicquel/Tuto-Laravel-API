71f335d0cb68e40a05a28f0e9249bd8d9c4775d3
myrdinaptaliesen <64499780+myrdinaptaliesen@users.noreply.github.com>
2022-09-06 10:49:30 +0200

    Mise en place de l'authentification avec JWT

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,38 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\API\ClubController;
 use App\Http\Controllers\API\PlayerController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+
 });
 
-Route::apiResource("players", PlayerController::class);
+Route::controller(PlayerController::class)->group(function () {
+    Route::get('players', 'index');
+    Route::post('player', 'store')->middleware('auth:api');
+    Route::get('player/{id}', 'show');
+    Route::patch('player/{id}', 'update')->middleware('auth:api');
+    Route::delete('player/{id}', 'destroy')->middleware('auth:api');
+}); 
 
-Route::apiResource("clubs", ClubController::class);
+Route::controller(ClubController::class)->group(function () {
+    Route::get('clubs', 'index');
+    Route::post('club', 'store')->middleware('auth:api');
+    Route::get('club/{id}', 'show');
+    Route::put('club/{id}', 'update')->middleware('auth:api');
+    Route::delete('club/{id}', 'destroy')->middleware('auth:api');
+}); 
+
+// Route::apiResource("players", PlayerController::class);
+
+// Route::apiResource("clubs", ClubController::class);
